87bb67d58de2177e25b69077e087c88d5c8d8a32
Olena Tatyuk <tatyukolena@gmail.com>
2025-05-12 11:51:27 -0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+forms.forEach((form) => {
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach((input) => {
+    const inputName = input.getAttribute('name');
+
+    const id = input.getAttribute('id');
+
+    const container = input.parentElement;
+
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+    label.setAttribute('for', id);
+    label.textContent = inputName.charAt(0).toUpperCase() + inputName.slice(1);
+
+    input.setAttribute('placeholder', label.textContent);
+
+    container.insertBefore(label, input);
+  });
+});
